c5b94f2b8f93c46bf9ebee0e21ce71025f6f09bd
Carlbrr <90042145+Carlbrr@users.noreply.github.com>
2024-04-05 12:17:11 +0200

    Added new prometheus metrics to be used for monitoring: (#51)
    * Added new prometheus metrics to be used for monitoring:
    
    Co-authored-by: stender98 <stender98@gmail.com>
    
    * fix formatting (:
    
    Co-authored-by: stender98 <stender98@gmail.com>
    
    * fix formatting (:
    
    Co-authored-by: stender98 <stender98@gmail.com>
    
    ---------
    
    Co-authored-by: stender98 <stender98@gmail.com>

--- api/main.go
+++ api/main.go
@@ -18,73 +18,69 @@ import (
 	"github.com/joho/godotenv"
 )
 
-
 var (
-  err error
-  env string
+	err error
+	env string
 )
 
-
 func main() {
-  flag.StringVar(&env, "env", "dev", "Environment to run the server in")
-  flag.Parse()
-  if env == "test" {
-    _, err = os.Stat(dbPath)
-    if err != nil {
-      initDB();
-    }
-  }
-  if env == "dev" {
-    if err := godotenv.Load(); err != nil {
-      log.Print("No .env file found")
-    }
-  }
+	flag.StringVar(&env, "env", "dev", "Environment to run the server in")
+	flag.Parse()
+	if env == "test" {
+		_, err = os.Stat(dbPath)
+		if err != nil {
+			initDB()
+		}
+	}
+	if env == "dev" {
+		if err := godotenv.Load(); err != nil {
+			log.Print("No .env file found")
+		}
+	}
 
-  reg := prometheus.NewRegistry()
-  reg.MustRegister(totalRequests, databaseAccesses, totalErrors)
-  promHandler := promhttp.HandlerFor(reg, promhttp.HandlerOpts{})
+	reg := prometheus.NewRegistry()
+	reg.MustRegister(totalRequests, databaseAccesses, totalErrors, totalGetUserIDRequests, unsuccessfulGetUserIDRequests, totalFollowRequests, unsuccessfulFollowRequests, totalUnfollowRequests, unsuccessfulUnfollowRequests, totalTweetMessageRequests, unsuccessfulTweetMessageRequests, notFound, badRequest, internalServerError)
+	promHandler := promhttp.HandlerFor(reg, promhttp.HandlerOpts{})
 
-  r := mux.NewRouter()
-  r.Path("/metrics").Handler(promHandler)
-  r.HandleFunc("/latest", getLatestHandler).Methods("GET")
-  r.HandleFunc("/register", registerHandler).Methods("POST")
-  r.HandleFunc("/login", loginHandler).Methods("POST")
-  r.HandleFunc("/msgs", msgsHandler).Methods("GET")
-  r.HandleFunc("/msgs/{username}", messagesPerUserHandler).Methods("GET", "POST")
-  r.HandleFunc("/msgsMy/{username}", msgsPersonalHandler).Methods("GET")
-  r.HandleFunc("/fllws/{username}", fllwsUserHandler).Methods("GET", "POST")
-  r.HandleFunc("/fllws/{whoUsername}/{whomUsername}", doesFllwUserHandler).Methods("GET")
-  r.HandleFunc("/userID/{username}", getUserIDHandler).Methods("GET")
-  r.HandleFunc("/user/{userID}", getUserHandler).Methods("GET")
+	r := mux.NewRouter()
+	r.Path("/metrics").Handler(promHandler)
+	r.HandleFunc("/latest", getLatestHandler).Methods("GET")
+	r.HandleFunc("/register", registerHandler).Methods("POST")
+	r.HandleFunc("/login", loginHandler).Methods("POST")
+	r.HandleFunc("/msgs", msgsHandler).Methods("GET")
+	r.HandleFunc("/msgs/{username}", messagesPerUserHandler).Methods("GET", "POST")
+	r.HandleFunc("/msgsMy/{username}", msgsPersonalHandler).Methods("GET")
+	r.HandleFunc("/fllws/{username}", fllwsUserHandler).Methods("GET", "POST")
+	r.HandleFunc("/fllws/{whoUsername}/{whomUsername}", doesFllwUserHandler).Methods("GET")
+	r.HandleFunc("/userID/{username}", getUserIDHandler).Methods("GET")
+	r.HandleFunc("/user/{userID}", getUserHandler).Methods("GET")
 
-  fmt.Println("Server is running on port 5001")
-  r.Use(beforeRequest)
-  http.ListenAndServe(":5001", r)
+	fmt.Println("Server is running on port 5001")
+	r.Use(beforeRequest)
+	http.ListenAndServe(":5001", r)
 }
-
 
 func beforeRequest(next http.Handler) http.Handler {
-  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    // Logic to be executed before passing the request to the main handler
-    db, err = connectDB()
-    if err != nil {
-      http.Error(w, err.Error(), http.StatusInternalServerError)
-      return
-    }
-    defer db.Close()
-    // Pass the request to the next handler in the chain
-    next.ServeHTTP(w, r)
-  }) 
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Logic to be executed before passing the request to the main handler
+		db, err = connectDB()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer db.Close()
+		// Pass the request to the next handler in the chain
+		next.ServeHTTP(w, r)
+	})
 }
 
-
-func notReqFromSimulator(w http.ResponseWriter, r *http.Request) (bool) {
-  fromSimulator := r.Header.Get("Authorization")
-  if false && fromSimulator != "Basic c2ltdWxhdG9yOnN1cGVyX3NhZmUh" {
-    errMsg := "You are not authorized to use this resource!"
-    w.WriteHeader(http.StatusUnauthorized)
-    io.WriteString(w, fmt.Sprintf(`{"status": 403, "error_msg": "%v"}`, errMsg))
-    return true
-  }
-  return false
+func notReqFromSimulator(w http.ResponseWriter, r *http.Request) bool {
+	fromSimulator := r.Header.Get("Authorization")
+	if false && fromSimulator != "Basic c2ltdWxhdG9yOnN1cGVyX3NhZmUh" {
+		errMsg := "You are not authorized to use this resource!!"
+		w.WriteHeader(http.StatusUnauthorized)
+		io.WriteString(w, fmt.Sprintf(`{"status": 403, "error_msg": "%v"}`, errMsg))
+		return true
+	}
+	return false
 }
--- api/prometheus.go
+++ api/prometheus.go
@@ -6,43 +6,116 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-
 var totalRequests = prometheus.NewCounter(
-  prometheus.CounterOpts{
-    Namespace: "api",
-    Name: "http_requests_total",
-    Help: "Number of get requests.",
-  },
+	prometheus.CounterOpts{
+		Namespace: "api",
+		Name:      "http_requests_total",
+		Help:      "Number of get requests.",
+	},
 )
 
 var databaseAccesses = prometheus.NewCounter(
-  prometheus.CounterOpts{
-    Namespace: "api",
-    Name: "database_accesses_total",
-    Help: "Amount of database accesses or operations",
-  },
+	prometheus.CounterOpts{
+		Namespace: "api",
+		Name:      "database_accesses_total",
+		Help:      "Amount of database accesses or operations",
+	},
 )
 
 var totalErrors = prometheus.NewCounter(
-  prometheus.CounterOpts{
-    Namespace: "api",
-    Name: "errors_total",
-    Help: "Amount of errors",
-  },
+	prometheus.CounterOpts{
+		Namespace: "api",
+		Name:      "errors_total",
+		Help:      "Amount of errors",
+	},
 )
 
-var loginRequests = prometheus.NewCounter(
-  prometheus.CounterOpts{
-    Namespace: "test1",
-    Name: "login_requests",
-    Help: "The amount of successful login requests on the web app",
-  },
+// add counters for requests and errors for the most used endpoints according to database cluster statistics.
+
+// This is the counter for the register requests
+// Register is the only endpoint that calls the getUserID function in the API
+var totalGetUserIDRequests = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Namespace: "api",
+		Name:      "get_user_id_requests",
+		Help:      "The amount of get user id requests on the api - for API this is only the Register that calls it",
+	},
+)
+var unsuccessfulGetUserIDRequests = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Namespace: "api",
+		Name:      "get_user_id_requests_failed",
+		Help:      "The amount of unsuccessful get user id requests on the api",
+	},
 )
 
-var unsuccessfulLoginRequests = prometheus.NewCounter(
-  prometheus.CounterOpts{
-    Namespace: "test1",
-    Name: "login_requests_failed",
-    Help: "The amount of unsuccessful login requests on the web app",
-  },
+var totalFollowRequests = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Namespace: "api",
+		Name:      "follow_requests",
+		Help:      "The amount of follow requests on the api",
+	},
+)
+var unsuccessfulFollowRequests = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Namespace: "api",
+		Name:      "follow_requests_failed",
+		Help:      "The amount of unsuccessful follow requests on the api",
+	},
+)
+
+var totalUnfollowRequests = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Namespace: "api",
+		Name:      "unfollow_requests",
+		Help:      "The amount of unfollow requests on the api",
+	},
+)
+
+var unsuccessfulUnfollowRequests = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Namespace: "api",
+		Name:      "unfollow_requests_failed",
+		Help:      "The amount of unsuccessful unfollow requests on the api",
+	},
+)
+
+var totalTweetMessageRequests = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Namespace: "api",
+		Name:      "post_message_requests",
+		Help:      "The amount of post message requests on the api",
+	},
+)
+
+var unsuccessfulTweetMessageRequests = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Namespace: "api",
+		Name:      "post_message_requests_failed",
+		Help:      "The amount of failed post message requests on the api",
+	},
+)
+
+var notFound = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Namespace: "api",
+		Name:      "not_found",
+		Help:      "The amount of not found requests on the api - 404 error",
+	},
+)
+
+var badRequest = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Namespace: "api",
+		Name:      "bad_request",
+		Help:      "The amount of bad request requests on the api - 400 error",
+	},
+)
+
+var internalServerError = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Namespace: "api",
+		Name:      "internal_server_error",
+		Help:      "The amount of internal server error requests on the api - 500 error",
+	},
 )
